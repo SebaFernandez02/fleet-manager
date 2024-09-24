6ec76d274a434c70bfe5a1ded6207ce03394380a
fraruiz <franciscoruizlezcano@gmail.com>
2024-09-24 18:26:20 -0300

    fix: remove id

--- src/main/java/ar/edu/ungs/fleet_manager/providers/application/ProviderRequest.java
+++ src/main/java/ar/edu/ungs/fleet_manager/providers/application/ProviderRequest.java
@@ -1,9 +1,8 @@
 package ar.edu.ungs.fleet_manager.providers.application;
 
-public record ProviderRequest(String id,
-                             String name,
-                             String email,
-                             String cuit,
-                             String phoneNumber,
-                             String address) {
+public record ProviderRequest(String name,
+                              String email,
+                              String cuit,
+                              String phoneNumber,
+                              String address) {
 }
