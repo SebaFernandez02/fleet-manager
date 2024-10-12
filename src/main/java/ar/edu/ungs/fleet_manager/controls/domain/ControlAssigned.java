52d4b5459c361d80bafe61013a48b62516b0054f
Sebastián Fernández <111531796+SebaFernandez02@users.noreply.github.com>
2024-10-12 17:27:05 -0300

    Update src/main/java/ar/edu/ungs/fleet_manager/controls/domain/ControlAssigned.java
    Co-authored-by: Francisco Ruiz <franciscoruizlezcano@gmail.com>

--- src/main/java/ar/edu/ungs/fleet_manager/controls/domain/ControlAssigned.java
+++ src/main/java/ar/edu/ungs/fleet_manager/controls/domain/ControlAssigned.java
@@ -2,11 +2,5 @@ package ar.edu.ungs.fleet_manager.controls.domain;
 
 import ar.edu.ungs.fleet_manager.shared.domain.exceptions.InvalidParameterException;
 
-public record ControlAssigned (String value){
-
-
-    @Override
-    public String value() {
-        return value;
-    }
+public record ControlAssigned(String value) {
 }
