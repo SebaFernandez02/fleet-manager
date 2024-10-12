79572931fa06ec44baec178c4fcd702d6e444bb4
fraruiz <franciscoruizlezcano@gmail.com>
2024-10-12 13:46:12 -0300

    feat: add validation to not create duplicate alerts

--- src/main/java/ar/edu/ungs/fleet_manager/alerts/domain/services/AlertCreator.java
+++ src/main/java/ar/edu/ungs/fleet_manager/alerts/domain/services/AlertCreator.java
@@ -1,5 +1,6 @@
 package ar.edu.ungs.fleet_manager.alerts.domain.services;
 
+import ar.edu.ungs.fleet_manager.alerts.application.search.AlertsSearcher;
 import ar.edu.ungs.fleet_manager.alerts.domain.Alert;
 import ar.edu.ungs.fleet_manager.alerts.domain.AlertRepository;
 import ar.edu.ungs.fleet_manager.alerts.domain.AlertStrategy;
@@ -9,14 +10,24 @@ import org.springframework.stereotype.Component;
 @Component
 public final class AlertCreator {
     private final AlertRepository repository;
+    private final AlertsSearcher searcher;
 
-    public AlertCreator(AlertRepository repository) {
+    public AlertCreator(AlertRepository repository, AlertsSearcher searcher) {
         this.repository = repository;
+        this.searcher = searcher;
     }
 
     public void execute(AlertStrategy strategy, VehicleId vehicleId) {
         Alert alert = Alert.factory(strategy, vehicleId.value());
 
-        this.repository.save(alert);
+        if (!alreadyContainsActiveAlert(alert)) {
+            this.repository.save(alert);
+        }
+    }
+
+    private boolean alreadyContainsActiveAlert(Alert alert) {
+        return this.searcher.execute().stream().filter(x -> !x.acknowledge())
+                .filter(x -> x.description().equals(alert.description().value()))
+                .anyMatch(x -> x.title().equals(alert.title().value()));
     }
 }
